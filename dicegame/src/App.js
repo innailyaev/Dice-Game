bda3f263eec0a057b16f7e39aa363e4d7e791534
Inna Ilyaev <inna4ilyaev@gmail.com>
2021-03-20 23:15:51 +0200

    20/03/21#6

--- dicegame/src/App.js
+++ dicegame/src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import GameBoard from "./Components/GameBoard.jsx";
+import "./Components/diceWhite.png"
 
 function App() {
   return (
     <div className="App">
-      <GameBoard/>
+      <div className="titleImage"><h1>Dice Game</h1><div className="diceImg"></div></div>
+     <GameBoard/>
+     <div className="whiteDiceImg"></div>
     </div>
   );
 }
